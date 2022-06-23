0ec425600df8becb5e4d634fd411e9bc51e36f4f
Nrulafind <mnurula.nu246@gmail.com>
2022-06-23 11:05:07 +0700

    arifin_wishlist_part07

---
+++ app/src/main/java/com/homindolentrahar/moment/detailTransaction.kt
@@ -0,0 +1,11 @@
+package com.homindolentrahar.moment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailTransaction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_transaction)
+    }
+}
